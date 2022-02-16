601203441ebae3ad0d5cfdcbe25a93b7a48cb10b
Trevor Mounts <trevormounts@gmail.com>
2022-02-16 12:09:19 -0500

    Added Test AI Store build (Android). Performed crawl and labeled elements for scripting tests. Built 'Custom Swipes' test.

---
+++ 1a9043d7-3e82-4d71-b035-4e553eb9a9d3/lkg/scripts/custom_swipe_on_coordinates.py
@@ -0,0 +1,7 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def run(context):
+    context.perform_gesture_on_coord('swipe_custom', {'start_x': 1000, 'start_y': 1400, 'end_x': 400, 'end_y': 1400, 'duration': 2000})
