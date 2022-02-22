bb0a793f560d3db6ca16cf88ad0d3668d1781a23
Alex Chappo <alexchappo@gmail.com>
2022-02-22 09:49:07 -0500

    updated persistent value passing step descriptions. Updated cookie script templates and added steps to cookie test.

--- apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/share_application_set.py
+++ apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/share_application_set.py
@@ -6,23 +6,19 @@ log = logging.getLogger(__name__)
 
 def run(context):
 
-# 1: capture data from the current screen:
-
     # get text values from a pricing table. 
     # use `dynamic=True` to allow capture of an element where the text might change,
     # as with shopping cart totals, new user IDs, etc...
     btn_text = context.find_element('btn_change', dynamic=True).get('text')
 
-# 2: access context datastore and add data for use in subsequent test steps:
-    # access the internal test case datastore (part of the `context` object)
-        # write out the text to the log to make sure value stored is correct if needed.
+
     log.info('====================output find element====================')
     log.info(btn_text)
     log.info('====================end find element====================')
     
     script_vals = context.get_test_script_vals()
 
-    # add element data to the TC datastore for use in subsequent steps
+    # add element data for use in subsequent steps
     script_vals['btn_info'] = btn_text
     #refresh page
     context.perform_gesture('tap', 'lnk_dynamic')
