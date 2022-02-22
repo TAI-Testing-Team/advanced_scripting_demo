d341847300c2ce05af172dc6afd957c957922303
Nicholas Mullins <nicholasmullins@gmail.com>
2022-02-22 16:21:11 +0200

    added conditions test and script, still having issues with the script pushing the buttons, but it is closing out the modal

--- apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/preconditions.py
+++ apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/preconditions.py
@@ -1,21 +1,24 @@
+import logging
+
 def run(context):
-   def dismiss_modal():
-       if _survey_iframe_exists(context):
-           context.perform_gesture('tap', 'no_thanks_button')
+    def dismiss_modal():
+        if _modal_exists(context):
+            context.perform_gesture('tap', 'btn_no_thanks')
 
-   # Add precondition that checks for the existence of a modal dialog.
-   # The precondition logic dismisses the dialog.
-   context.preconditions.append(dismiss_modal)
+    # Add precondition that checks for the existence of a modal dialog.
+    # The precondition logic dismisses the dialog.
+    context.preconditions.append(dismiss_modal)
 
-   # Prior to executing each step, the precondition function will be executed.
-   # This ensures the modal dialog is dismissed prior to gesture execution.
-   context.perform_gesture('tap', 'deals_menu')
-   context.perform_gesture('tap', 'laptop_deals_menu')
+    # Prior to executing each step, the precondition function will be executed.
+    # This ensures the modal dialog is dismissed prior to gesture execution.
+    context.perform_gesture('tap', 'btn_free')
+    context.perform_gesture('tap', 'btn_paid')
 
 
-def _survey_iframe_exists(context):
-   try:
-       context.get_driver().find_element_by_id('iframeSurvey')
-       return True
-   except:
-       return False
+
+def _modal_exists(context):
+    try:
+        context.get_driver().find_element_by_id('sign_up_modal')
+        return True
+    except:
+        return False
