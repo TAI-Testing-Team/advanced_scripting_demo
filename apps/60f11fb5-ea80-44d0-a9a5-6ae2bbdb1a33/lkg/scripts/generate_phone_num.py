396168bb1c427e17eb5e5eb69265ca83b51bebb0
Trevor Mounts <trevormounts@gmail.com>
2022-02-23 12:42:46 -0500

    Turned off Run Explore-Bot Constraints, Test Hooks -On After Launch+On Before Retry, and Run Explore-Bot Constraints Android.

--- apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/generate_phone_num.py
+++ apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/generate_phone_num.py
@@ -16,22 +16,23 @@ random.seed(time.perf_counter())
 
 def run(context):
     # gather info
-    us_area_code_cities = context.get_file_path("~/test_ai/apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/us-area-code-cities.csv`")
+    us_area_code_cities = context.get_file_path("us-area-code-cities.csv")
+    
     # find a valid area code
     with open(us_area_code_cities) as f:
     	reader = csv.reader(f)
     	data = [row[col] for row in reader]
     
     # generate valid phone number and take action
-    if element_exists(context, 'inp_phone_number'):
+    if element_exists(context, 'inp_text_entry'):
         random_area_code = random.choice(data)
         random_exchange = random.randint(200, 999)
         random_number = random.randint(1, 9999)
         random_phone_number = '{0}{1:03d}{2:04d}'.format(random_area_code, random_exchange, random_number)
         context.perform_gesture('text_entry_no_submit', 'inp_text_entry', random_phone_number)
-        #context.verify(grep=random_phone_number)
-        #context.perform_gesture('tap', 'btn_submit')
-
+        context.verify(labels=["random_phone_number"])
+        context.perform_gesture('tap', 'btn_submit')
+        
 def element_exists(context, label):
     labels_per_elem = context.find_elements_with_label(label)
     return labels_per_elem and len(labels_per_elem.keys()) > 0
--- apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/verify_explore_constraints_b.py
+++ apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/verify_explore_constraints_b.py
@@ -1,38 +1,47 @@
 import logging
+log = logging.getLogger(__name__)
 import os
 
 def run(context):
-    if context.verify(labels=["scrn_home"]):
+    #verified=context.verify(grep="Please Select One")
+    home_screen = context.element_exists("txt_please_select_test_start")
+    basic_gestures = context.element_exists("btn_double_tap")
+    find_element = context.element_exists("txt_please_select_test_start")
+    log.info("=======================Verified=========================")
+    log.info(element)
+    log.info("=======================Verified=========================")
+    #if verified == True: 
+    if home_screen==True or basic_gestures==True or find_element==True:
         context.perform_gesture('tap', 'lnk_find_element')
         context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified')
     
-    elif context.verify(labels=["scrn_basic_gestures"]):
-        context.perform_gesture('tap', 'lnk_find_element')
-        context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ')        
+    # elif context.verify(labels=["scrn_basic_gestures"]):
+    #     context.perform_gesture('tap', 'lnk_find_element')
+    #     context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ')        
     
-    elif context.verify(labels=["scrn_tap_parameterization"]):
-        context.perform_gesture('tap', 'lnk_find_element')
-        context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
+    # elif context.verify(labels=["scrn_tap_parameterization"]):
+    #     context.perform_gesture('tap', 'lnk_find_element')
+    #     context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
     
-    elif context.verify(labels=["scrn_wait"]):
-        context.perform_gesture('tap', 'lnk_find_element')
-        context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
+    # elif context.verify(labels=["scrn_wait"]):
+    #     context.perform_gesture('tap', 'lnk_find_element')
+    #     context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
     
-    elif context.verify(labels=["scrn_dynamic"]):
-        context.perform_gesture('tap', 'lnk_find_element')
-        context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
+    # elif context.verify(labels=["scrn_dynamic"]):
+    #     context.perform_gesture('tap', 'lnk_find_element')
+    #     context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
     
-    elif context.verify(labels=["scrn_verify"]):
-        context.perform_gesture('tap', 'lnk_find_element')
-        context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
+    # elif context.verify(labels=["scrn_verify"]):
+    #     context.perform_gesture('tap', 'lnk_find_element')
+    #     context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
     
-    elif context.verify(labels=["scrn_basic_gestures"]):
-        context.perform_gesture('tap', 'lnk_find_element')
-        context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
+    # elif context.verify(labels=["scrn_basic_gestures"]):
+    #     context.perform_gesture('tap', 'lnk_find_element')
+    #     context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ') 
     
-    elif context.verify(labels=["scrn_input_parameterization"]):
-        context.perform_gesture('tap', 'lnk_find_element')
-        context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ')     
+    # elif context.verify(labels=["scrn_input_parameterization"]):
+    #     context.perform_gesture('tap', 'lnk_find_element')
+    #     context.perform_gesture('text_entry_no_submit', 'inp_ta_element_info', 'Explore mode bot constraints verified ')     
     
     else:   
         context.perform_gesture('tap', 'lnk_find_element')
