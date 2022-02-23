4851476ff02d8065037fc47b84056530999f2291
Alex Chappo <alexchappo@gmail.com>
2022-02-23 08:22:41 -0500

    Committing again from last night. Updated basic gestures script and tap on coordinate script to have logic that pulls the correct coordinate to tap out, so it isn't resolution specific.

--- apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/before_each_explore_step.py
+++ apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/before_each_explore_step.py
@@ -2,9 +2,12 @@ import logging
 
 log = logging.getLogger(__name__)
 
-def before_each_step():
-        properties['num_explore_steps'] += 1
-        log.info("Running exploration step: {}".format(properties['num_explore_steps']))
-        driver.launch_app(http://blogen-inc.surge.sh)
+def run(context):
+        driver = context.get_driver()
+        properties = []
+        def before_each_step():
+                properties['num_explore_steps'] += 1
+                log.info("Running exploration step: {}".format(properties['num_explore_steps']))
+                driver.get('http://blogen-inc.surge.sh')
 
-context.explore(20, before_each_step=before_each_step, ignore_element_errors=True)
+        context.explore(20, before_each_step=before_each_step, ignore_element_errors=True)
--- apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/tap_custom_coordinates.py
+++ apps/60f11fb5-ea80-44d0-a9a5-6ae2bbdb1a33/lkg/scripts/tap_custom_coordinates.py
@@ -3,6 +3,31 @@ import logging
 log = logging.getLogger(__name__)
 
 def run(context):
-    context.perform_gesture_on_coord('tap', {'x': 509, 'y': 650})
+    coordinate_tap_info = context.find_element('btn_coordinate_tap')
+    # put the properties into variables that can be used
+    cord_x = coordinate_tap_info.get('x')
+    cord_y = coordinate_tap_info.get('y')
+    cord_w = coordinate_tap_info.get('width')
+    cord_h = coordinate_tap_info.get('height')
+    cord_cx = cord_x + (cord_w / 2)
+    cord_cy = cord_y + (cord_h / 2)
+
+
+    log.info('=========================info======================')
+    log.info(cord_x)
+    log.info(cord_y)
+    log.info(cord_w)
+    log.info(cord_h)
+    log.info('=======coordinate tap=========')
+    log.info(cord_cx)
+    log.info(cord_cy)
+
+
+
+
+
+    log.info('=========================info======================')
+
+    context.perform_gesture_on_coord('tap', {'x': cord_cx, 'y': cord_cy})
     context.verify(grep="Coordinate Tap Successful")
 
